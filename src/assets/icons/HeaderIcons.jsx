26770c07eec5eebc5263abb53e687cd3d111878e
NahuelEF <nahuelfedele@gmail.com>
2023-08-27 02:53:11 -0300

    fix: header and sidebar icons

--- src/assets/icons/HeaderIcons.jsx
+++ src/assets/icons/HeaderIcons.jsx
@@ -1,35 +1,35 @@
 export const IconClose = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16}>
     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
   </svg>
 );
 
 export const IconDot = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={4} height={15} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={4} height={15}>
     <path d="M2.39 12c.83 0 1.5.67 1.5 1.5S3.22 15 2.39 15s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5ZM.89 7.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5S3.22 6 2.39 6s-1.5.67-1.5 1.5Zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5S3.22 0 2.39 0 .89.67.89 1.5Z" />
   </svg>
 );
 
 export const IconList = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={14} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={14}>
     <path d="M18 1.5H0v-1h18v1Zm0 5H0v1h18v-1Zm0 6H0v1h18v-1Z" />
   </svg>
 );
 
 export const IconMicrophone = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={14} height={19} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={14} height={19}>
     <path d="M7 .5C5.34.5 4 1.87 4 3.57v5.86c0 1.7 1.34 3.07 3 3.07s3-1.37 3-3.07V3.57C10 1.87 8.66.5 7 .5Zm6.5 9h-1c0 3.03-2.47 5.5-5.5 5.5a5.51 5.51 0 0 1-5.5-5.5h-1c0 3.24 2.39 5.93 5.5 6.41v2.59h2v-2.59c3.11-.48 5.5-3.17 5.5-6.41Z" />
   </svg>
 );
 
 export const IconPerson = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={21} height={21} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={21} height={21}>
     <path d="M10.39.5c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10Zm0 1c4.96 0 9 4.04 9 9 0 1.42-.34 2.76-.93 3.96-1.53-1.72-3.98-2.89-7.38-3.03a3.995 3.995 0 0 0-.69-7.93c-2.21 0-4 1.79-4 4 0 1.97 1.43 3.6 3.31 3.93-3.4.14-5.85 1.31-7.38 3.03-.59-1.2-.93-2.54-.93-3.96 0-4.96 4.04-9 9-9Zm-3 6c0-1.65 1.35-3 3-3s3 1.35 3 3-1.35 3-3 3-3-1.35-3-3Zm3 12c-3.16 0-5.94-1.64-7.55-4.12C4.4 13.43 7 12.4 10.39 12.4c3.39 0 5.99 1.03 7.55 2.98-1.61 2.48-4.39 4.12-7.55 4.12Z" />
   </svg>
 );
 
 export const IconSearch = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={19} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={19}>
     <path d="m17.87 17.67-5.59-5.59A6.956 6.956 0 0 0 14 7.5c0-3.87-3.13-7-7-7s-7 3.13-7 7a6.995 6.995 0 0 0 11.58 5.29l5.59 5.59.7-.71ZM7 13.5c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6Z" />
   </svg>
 );
--- src/assets/icons/SidebarIcons.jsx
+++ src/assets/icons/SidebarIcons.jsx
@@ -1,29 +1,29 @@
 export const IconBag = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={20}>
     <path d="M3 6c0 2.76 2.24 5 5 5s5-2.24 5-5h-1c0 2.21-1.79 4-4 4S4 8.21 4 6H3Zm9.9-2a5 5 0 0 0-9.8 0H0v14c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4h-3.1ZM8 1c1.86 0 3.43 1.27 3.87 3H4.13C4.57 2.27 6.14 1 8 1Zm7 17c0 .55-.45 1-1 1H2c-.55 0-1-.45-1-1V5h14v13Z" />
   </svg>
 );
 
 export const IconBroadcast = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
     <path d="M14 12c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2ZM8.48 8.45l-.71-.7A5.976 5.976 0 0 0 6 12c0 1.66.68 3.17 1.77 4.25l.71-.71C7.57 14.64 7 13.39 7 12c0-1.39.57-2.64 1.48-3.55Zm7.75-.7-.71.71c.91.9 1.48 2.15 1.48 3.54 0 1.39-.57 2.64-1.48 3.55l.71.71A6.017 6.017 0 0 0 18 12c0-1.66-.68-3.17-1.77-4.25ZM5.65 5.63l-.7-.71a9.968 9.968 0 0 0 0 14.16l.71-.71A8.954 8.954 0 0 1 3 12c0-2.49 1.02-4.74 2.65-6.37Zm13.4-.71-.71.71A8.954 8.954 0 0 1 21 12c0 2.49-1.02 4.74-2.65 6.37l.71.71C20.87 17.27 22 14.76 22 12s-1.13-5.27-2.95-7.08Z" />
   </svg>
 );
 
 export const IconBroadcastPin = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20}>
     <path d="M4 10c0-3.31 2.69-6 6-6s6 2.69 6 6c0 1.66-.67 3.16-1.77 4.25l-.71-.71C14.44 12.63 15 11.38 15 10c0-2.76-2.24-5-5-5s-5 2.24-5 5c0 1.38.56 2.63 1.47 3.54l-.71.71A5.985 5.985 0 0 1 4 10Zm8 0c0-1.1-.9-2-2-2s-2 .9-2 2c0 .74.4 1.38 1 1.72V20h2v-8.28c.6-.34 1-.98 1-1.72Zm-9.06 7.08.71-.71A8.945 8.945 0 0 1 1 10c0-4.96 4.04-9 9-9s9 4.04 9 9c0 2.49-1.01 4.74-2.65 6.37l.71.71A9.942 9.942 0 0 0 20 10c0-5.52-4.48-10-10-10S0 4.48 0 10c0 2.77 1.12 5.27 2.94 7.08Z" />
   </svg>
 );
 
 export const IconClothes = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={20}>
     <path d="M8.5 4.44v-.5C9.36 3.71 10 2.93 10 2c0-1.1-.9-2-2-2S6 .9 6 2h1c0-.55.45-1 1-1s1 .45 1 1-.45 1-1 1h-.5v1.44L0 11h2v6h1v2h1v-2h2v3h1v-3h2v2h1v-2h1v-3h3v-3h2L8.5 4.44ZM2.66 10 8 5.33 13.34 10H2.66ZM10 16H3v-5h7v5Zm1-3v-2h2v2h-2Z" />
   </svg>
 );
 
 export const IconCollectionPlay = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={18} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={18}>
     <path d="M8 15V9l5 3-5 3Zm7-15H5v1h10V0Zm3 3H2v1h16V3Zm2 3H0v12h20V6ZM1 7h18v10H1V7Z" />
   </svg>
 );
@@ -35,67 +35,91 @@ export const IconFire = () => (
 );
 
 export const IconFilm = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={20}>
     <path d="m21.01 3.91-.5-2.96L.64 4.19 1 7v13h20V7H2.06l18.95-3.09ZM4 8l1 3h3L7 8h2l1 3h3l-1-3h2l1 3h3l-1-3h3v11H2V8h2Z" />
   </svg>
 );
 
 export const IconGaming = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={16} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={16}>
     <path d="M8 8H6v2H4V8H2V6h2V4h2v2h2v2Zm7 .5c0-.83-.67-1.5-1.5-1.5S12 7.67 12 8.5s.67 1.5 1.5 1.5S15 9.33 15 8.5Zm3-3c0-.83-.67-1.5-1.5-1.5S15 4.67 15 5.5 15.67 7 16.5 7 18 6.33 18 5.5Zm-3.03-4.35-4.5 2.53-.49.27-.49-.27-4.5-2.53L1 3.39v6.43l8.98 5.04 8.98-5.04V3.39l-3.99-2.24Zm0-1.15 4.99 2.8v7.6L9.98 16 0 10.4V2.8L4.99 0l4.99 2.8L14.97 0Z" />
   </svg>
 );
 
 export const IconHistory = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
     <path d="M14.97 16.95 10 13.87V7h2v5.76l4.03 2.49-1.06 1.7ZM22 12c0 5.51-4.49 10-10 10S2 17.51 2 12h1c0 4.96 4.04 9 9 9s9-4.04 9-9-4.04-9-9-9C8.81 3 5.92 4.64 4.28 7.38c-.11.18-.22.37-.31.56L3.94 8H8v1H1.96V3h1v4.74c.04-.09.07-.17.11-.25.11-.22.23-.42.35-.63C5.22 3.86 8.51 2 12 2c5.51 0 10 4.49 10 10Z" />
   </svg>
 );
 
 export const IconHome = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24}>
     <path d="M4 21V10.08l8-6.96 8 6.96V21h-6v-6h-4v6H4Z" />
   </svg>
 );
 
 export const IconLibrary = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18}>
     <path d="m8 4 6 3.5L8 11V4Zm7 13H1V3H0v15h15v-1Zm3-2H3V0h15v15ZM4 14h13V1H4v13Z" />
   </svg>
 );
 
 export const IconLightbulb = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20} fill="currentColor">
-    <path d="M15 3V0H3v3H0v6l3.23 1.61c.7 2.5 2.97 4.34 5.69 4.38L5 17v3h8v-3l-3.92-2.01a5.998 5.998 0 0 0 5.69-4.38L18 9V3h-3ZM3 9.38l-2-1V4h2v5.38ZM12 19H6v-1.39l3-1.54 3 1.54V19Zm2-10c0 2.76-2.24 5-5 5s-5-2.24-5-5V1h10v8Zm3-.62-2 1V4h2v4.38Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" width={16} height={20}>
+    <path d="M12 19H9.72a1.98 1.98 0 0 1-3.44 0H4v-1h8v1Zm4-11a7.98 7.98 0 0 1-4 6.92V17H4v-2.08A7.98 7.98 0 0 1 0 8c0-4.42 3.58-8 8-8s8 3.58 8 8Zm-5 8v-1.66l.5-.29A7.017 7.017 0 0 0 15 8c0-3.86-3.14-7-7-7S1 4.14 1 8c0 2.48 1.34 4.8 3.5 6.06l.5.28V16h6Z" />
   </svg>
 );
 
 export const IconMusicNote = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={14} height={16} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={14} height={16}>
     <path d="M7 0v9.38C6.27 8.54 5.2 8 4 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V4h6V0H7ZM4 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3Zm9-12H8V1h5v2Z" />
   </svg>
 );
 
 export const IconNewspaper = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={18}>
     <path d="M8 8v6H4V8h4Zm1-1H3v8h6V7ZM0 .03V18h14l4-4V.03M17 1v11.99l-.01.01H13v3.99l-.01.01H1V1h16Zm-2 4H3V3h12v2Zm0 7h-5v-2h5v2Zm0-3h-5V7h5v2Z" />
   </svg>
 );
 
 export const IconPlus = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20}>
     <path d="M15 11h-4v4H9v-4H5V9h4V5h2v4h4v2ZM10 1c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9Zm0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S0 15.52 0 10 4.48 0 10 0Z" />
   </svg>
 );
 
 export const IconShorts = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20}>
     <path d="M7 12.65v-5.3L12 10l-5 2.65Zm7.77-4.33-1.2-.5L15 7.06A3.729 3.729 0 0 0 16.56 2 3.751 3.751 0 0 0 11.49.44L3 4.94a3.744 3.744 0 0 0-2 3.49c.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L3 12.93a3.751 3.751 0 0 0 3.51 6.63l8.5-4.5A3.736 3.736 0 0 0 17 11.57a3.762 3.762 0 0 0-2.23-3.25Zm-.23 5.86-8.5 4.5c-1.34.71-3.01.2-3.72-1.14-.71-1.34-.2-3.01 1.14-3.72l2.04-1.08v-1.21l-.69-.28-1.11-.46A2.751 2.751 0 0 1 2 8.38c-.05-1.06.52-2.06 1.46-2.56l8.5-4.5c1.34-.71 3.01-.2 3.72 1.14.71 1.34.2 3.01-1.14 3.72L12.5 7.26v1.21l1.8.74c.99.41 1.65 1.35 1.7 2.41.05 1.06-.52 2.06-1.46 2.56Z" />
   </svg>
 );
 
 export const IconTrophy = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20} fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20}>
     <path d="M15 3V0H3v3H0v6l3.23 1.61c.7 2.5 2.97 4.34 5.69 4.38L5 17v3h8v-3l-3.92-2.01a5.998 5.998 0 0 0 5.69-4.38L18 9V3h-3ZM3 9.38l-2-1V4h2v5.38ZM12 19H6v-1.39l3-1.54 3 1.54V19Zm2-10c0 2.76-2.24 5-5 5s-5-2.24-5-5V1h10v8Zm3-.62-2 1V4h2v4.38Z" />
+  </svg>
+);
+
+export const IconSettings = () => (
+  <svg width={24} height={24}>
+    <path d="M12 9.5a2.5 2.5 0 0 1 0 5 2.5 2.5 0 0 1 0-5m0-1c-1.93 0-3.5 1.57-3.5 3.5s1.57 3.5 3.5 3.5 3.5-1.57 3.5-3.5-1.57-3.5-3.5-3.5zM13.22 3l.55 2.2.13.51.5.18c.61.23 1.19.56 1.72.98l.4.32.5-.14 2.17-.62 1.22 2.11-1.63 1.59-.37.36.08.51c.05.32.08.64.08.98s-.03.66-.08.98l-.08.51.37.36 1.63 1.59-1.22 2.11-2.17-.62-.5-.14-.4.32c-.53.43-1.11.76-1.72.98l-.5.18-.13.51-.55 2.24h-2.44l-.55-2.2-.13-.51-.5-.18c-.6-.23-1.18-.56-1.72-.99l-.4-.32-.5.14-2.17.62-1.21-2.12 1.63-1.59.37-.36-.08-.51c-.05-.32-.08-.65-.08-.98s.03-.66.08-.98l.08-.51-.37-.36L3.6 8.56l1.22-2.11 2.17.62.5.14.4-.32c.53-.44 1.11-.77 1.72-.99l.5-.18.13-.51.54-2.21h2.44M14 2h-4l-.74 2.96c-.73.27-1.4.66-2 1.14l-2.92-.83-2 3.46 2.19 2.13c-.06.37-.09.75-.09 1.14s.03.77.09 1.14l-2.19 2.13 2 3.46 2.92-.83c.6.48 1.27.87 2 1.14L10 22h4l.74-2.96c.73-.27 1.4-.66 2-1.14l2.92.83 2-3.46-2.19-2.13c.06-.37.09-.75.09-1.14s-.03-.77-.09-1.14l2.19-2.13-2-3.46-2.92.83c-.6-.48-1.27-.87-2-1.14L14 2z" />
+  </svg>
+);
+
+export const IconFlag = () => (
+  <svg width={24} height={24}>
+    <path d="m13.18 4 .24 1.2.16.8H19v7h-5.18l-.24-1.2-.16-.8H6V4h7.18M14 3H5v18h1v-9h6.6l.4 2h7V5h-5.6L14 3z" />
+  </svg>
+);
+
+export const IconHelp = () => (
+  <svg width={24} height={24}>
+    <path d="M15.36 9.96c0 1.09-.67 1.67-1.31 2.24-.53.47-1.03.9-1.16 1.6l-.04.2H11.1l.03-.28c.14-1.17.8-1.76 1.47-2.27.52-.4 1.01-.77 1.01-1.49a1.634 1.634 0 0 0-2.05-1.58c-.59.15-1.05.67-1.19 1.34l-.05.28H8.57l.06-.42c.2-1.4 1.15-2.53 2.42-2.87 1.05-.29 2.14-.08 2.98.57.85.64 1.33 1.62 1.33 2.68zM12 18c.55 0 1-.45 1-1s-.45-1-1-1-1 .45-1 1 .45 1 1 1zm0-15c-4.96 0-9 4.04-9 9s4.04 9 9 9 9-4.04 9-9-4.04-9-9-9m0-1c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2z" />
+  </svg>
+);
+
+export const IconFeedback = () => (
+  <svg width={24} height={24}>
+    <path d="M13 14h-2v-2h2v2zm0-9h-2v6h2V5zm6-2H5v16.59l3.29-3.29.3-.3H19V3m1-1v15H9l-5 5V2h16z" />
   </svg>
 );
