ba2d44e092105a40c043faa9127f607673885333
NahuelEF <nahuelfedele@gmail.com>
2023-08-30 00:38:08 -0300

    add: sidebar context

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Header } from "./components";
+import { SidebarContext } from "./contexts";
+import { Header, Sidebar } from "./layouts";
 import { Home } from "./pages";
 
 function App() {
+  const [openSidebar, setOpenSidebar] = useState(true);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <SidebarContext.Provider value={{ openSidebar, setOpenSidebar }}>
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </SidebarContext.Provider>
   );
 }
 
